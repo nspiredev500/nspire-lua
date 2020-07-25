7ef0968c31008d11dc0cefad8f5d92db70f2f5e3
nspiredev500 <57017842+nspiredev500@users.noreply.github.com>
2020-07-25 20:09:04 +0200

    started the window system

---
+++ window/test.lua
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+function on.construction()
+	
+	
+	
+	
+	platform.window:invalidate()
+end
+
+
+
+
+
+function on.paint(gc)
+	gc:setColorRGB(255,255,255)
+	--gc:fillRect(0,0,320,240)
+	
+	
+	
+	
+end
+
+
+
+
+
+
+
+
